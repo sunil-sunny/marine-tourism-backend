c44deb5cc38f834213ae395c60ae9ae99d88024a
sunil-sunny <sunilsunny183@gmail.com>
2020-11-22 13:02:24 -0400

    Register and Authentication finished

--- routes/profileManagementRoute.js
+++ routes/profileManagementRoute.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/profileManagementController')
 
+router.post('/register', registerUser);
 
-
-
-router.post('/register',async (req, res) => {
-
-    res.send("App Started")
-});
+router.post('/login', loginUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,48 +1,23 @@
-const express =  require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const profileMagementRoute=require('./routes/profileManagementRoute')
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var cors = require('cors')
+app.use(bodyParser.json());
+app.use(cors())
 
-// Our function which adds two numbers and returns the result
-const addNumbers = (firstNumber, secondNumber) => {
-//   check that input is a number
-  if (typeof(Number(firstNumber)) !== 'number' || typeof(Number(secondNumber)) !== 'number') {
-    return 'Values should be integer or numbers'
-  }
-  return Number(firstNumber) + Number(secondNumber);
-}
+//connect to DB
+mongoose
+    .connect('mongodb+srv://saisunil:mongo!123@cluster0-i4z56.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error(err));
+app.use('/user',profileMagementRoute)
 
-// Destructure our bodyParser methods
-const { urlencoded, json } = bodyParser;
-const port = process.env.PORT || 8080;
-// intialize our express app
-const app = express();
-
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(json());
-app.use(urlencoded({ extended: false }));
-
-// end point to add numbers
-app.post('/api/add', (req, res) => {
-  const { firstNumber, secondNumber } = req.body;
-  const result =  addNumbers(firstNumber, secondNumber);
-  return res.status(200).send({
-    result
-  });
-});
-// app entry point
-app.get('/', (req, res) => res.status(200).send({
-  message: 'Welcome to our glorious app',
-}));
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
-
-app.listen(port, (err) => {
-  if (!err) {
-     console.log(`App started on port ${port}`);
-  } else {
-    console.log(err);
-  }
-});
-
-module.exports = app;
+//Start the app by listening on the default Heroku port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
